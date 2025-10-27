72f9ac4430060da619954f7ba677a393cdbe3cb4
arturtrombim <arturtrombim.dev@gmail.com>
2025-10-27 16:15:02 -0300

    feature: adicionando botão ao cta

--- src/app/sections/cta/cta.ts
+++ src/app/sections/cta/cta.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Button } from "../../shared/button/button";
 
 @Component({
   selector: 'cta',
   standalone: true,
-  imports: [],
+  imports: [Button],
   templateUrl: './cta.html',
   styleUrl: './cta.css'
 })
