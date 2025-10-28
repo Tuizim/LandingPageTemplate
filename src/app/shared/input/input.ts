0513bdde43f06f83cdb11d5b2879a1de692fa527
arturtrombim <arturtrombim.dev@gmail.com>
2025-10-28 08:57:19 -0300

    feature: adicionar comportamento de campo invalido ao componente input

--- src/app/shared/input/input.ts
+++ src/app/shared/input/input.ts
@@ -4,10 +4,12 @@ import { Component, Input } from '@angular/core';
   selector: 'input-field',
   standalone: true,
   templateUrl: './input.html',
-  styleUrl: './input.css'
+  styleUrls: ['./input.css']
 })
 export class InputField {
   @Input() label = '';
   @Input() placeholder = '';
   @Input() type = 'text';
+  @Input() errorMessage = '';
+  @Input() invalid = false;
 }
